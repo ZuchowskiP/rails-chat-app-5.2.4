c88d33edfda2c4659fbbdad99b936c4819cd12d6
DESKTOP-50B3PKI\anubu <a.wolfus@gmail.com>
2022-10-08 15:45:30 +0200

    Added flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       flash[:success] = "You have successfully logged in"
       redirect_to root_path
     else
-      flash.now[:alert] = "Error invalid login information"
+      flash.now[:error] = "Error invalid login information"
       render 'new'
     end
   end
